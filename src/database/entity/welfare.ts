b5d6272b2c17803416ba30c7b103ad41ef30d7dd
pangcurry <magicshuffle2424@hotmail.com>
2021-01-11 15:38:00 +0900

    [Prevent] prevent welfare

--- src/database/entity/welfare.ts
+++ src/database/entity/welfare.ts
@@ -1,27 +1,27 @@
-import { Entity, PrimaryColumn, Column, OneToOne, JoinColumn } from "typeorm";
-import { Enterprise } from "./enterprise";
+// import { Entity, PrimaryColumn, Column, OneToOne, JoinColumn } from "typeorm";
+// import { Enterprise } from "./enterprise";
 
-@Entity()
-export class Welfare {
-  @PrimaryColumn({ length: 12, type: "char", name: "ent_no" })
-  entNo: string;
+// @Entity()
+// export class Welfare {
+//   @PrimaryColumn({ length: 12, type: "char", name: "ent_no" })
+//   entNo: string;
 
-  @Column({ type: "tinyint", default: false, name: "four_major" })
-  fourMajor: boolean;
+//   @Column({ type: "tinyint", default: false, name: "four_major" })
+//   fourMajor: boolean;
 
-  @Column({ type: "tinyint", default: false, name: "self_develop" })
-  selfDevelop: boolean;
+//   @Column({ type: "tinyint", default: false, name: "self_develop" })
+//   selfDevelop: boolean;
 
-  @Column({ type: "tinyint", default: false })
-  laptop: boolean;
+//   @Column({ type: "tinyint", default: false })
+//   laptop: boolean;
 
-  @Column({ nullable: true })
-  etc: string;
+//   @Column({ nullable: true })
+//   etc: string;
 
-  @OneToOne((type) => Enterprise, (enterprise) => enterprise.entNo, {
-    onUpdate: "CASCADE",
-    onDelete: "CASCADE",
-  })
-  @JoinColumn({ name: "ent_no" })
-  enterprise: Enterprise;
-}
+//   @OneToOne((type) => Enterprise, (enterprise) => enterprise.entNo, {
+//     onUpdate: "CASCADE",
+//     onDelete: "CASCADE",
+//   })
+//   @JoinColumn({ name: "ent_no" })
+//   enterprise: Enterprise;
+// }
