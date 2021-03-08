042115cb639e495225cca4922a1412567e9efca7
pangcurry <magicshuffle2424@hotmail.com>
2021-03-08 21:17:05 +0900

    [Fix] token key

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const tokenInfo = {
     issuer:"test",
     key: "어쩌구저쩌구복잡한키",
-    accessTokenValidity: 1000,
+    // accessTokenValidity: 1000,
+    accessTokenValidity:10000,
     refreshTokenValidity: 1000
 }
