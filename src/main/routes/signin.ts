7b00385ecdf004487760ea141bc96833bafb0242
pangcurry <magicshuffle2424@hotmail.com>
2021-03-09 15:28:15 +0900

    [Add] add adaptRoute, makeSigninController

--- src/main/routes/signin.ts
+++ src/main/routes/signin.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import { adaptRoute } from '../adapters';
+import { makeSigninController } from '../factories/controllers';
 
 export default (router: Router): void => {
-    router.post('/signin/basic');
+    router.post('/signin/basic',adaptRoute(makeSigninController()));
     router.post('/signin/admin');
 }
