d79881588ca9d80572370b4b63eccce59dd7ef19
pangcurry <magicshuffle2424@hotmail.com>
2021-03-09 15:29:44 +0900

    [Add] add HttpResponse format

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,28 @@
+import { HttpResponse } from '../../presentation/protocols';
+
+export const ok = (data: any): HttpResponse => {
+    return {
+        status: 200,
+        statusCode: "10000",
+        message: "Success",
+        data
+    };
+}
+
+export const badRequest = (error: Error): HttpResponse => {
+    return {
+        status: 400,
+        statusCode: error.name,
+        message: error.message,
+        data:{}
+    };
+}
+
+export const Unauthorized = (error: Error): HttpResponse => {
+    return {
+        status: 401,
+        statusCode: error.name,
+        message: error.message,
+        data:{}
+    };
+}
