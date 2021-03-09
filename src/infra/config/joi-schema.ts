bc5ebc43170ce73bc82c151ea7dceffd052294ec
pangcurry <magicshuffle2424@hotmail.com>
2021-03-09 22:46:07 +0900

    [Fix] Fix joi-schema add keys

--- src/infra/config/joi-schema.ts
+++ src/infra/config/joi-schema.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-export const schema = Joi.object({
-    sId:Joi.string().email().required(),
-    sPassword: Joi.string().required().min(6).max(20)
+export const schema = Joi.object().keys({
+    sId:Joi.string().min(7).email({ minDomainSegments:3, tlds: { allow: ['kr'] } }),
+    sPassword: Joi.string().min(6).max(20)
 });
