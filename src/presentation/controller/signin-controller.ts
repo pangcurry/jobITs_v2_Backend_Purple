5901189d0988ce98f1ebbc1fb43fbe02f2f85124
pangcurry <magicshuffle2424@hotmail.com>
2021-03-09 22:51:05 +0900

    [Add] add Error

--- src/presentation/controller/signin-controller.ts
+++ src/presentation/controller/signin-controller.ts
@@ -1,13 +1,27 @@
-import { Controller, HttpResponse } from "../protocols";
+import { ServerError } from "../errors/server-error";
+import { badRequest, ok, serverError } from "../helpers";
+import { Controller, HttpResponse, joiValidation } from "../protocols";
 
 export class SigninController implements Controller {
-    constructor() {}
+    constructor(
+        private readonly validation: joiValidation
+    ) {}
     async handle (request: SigninController.Request): Promise<HttpResponse> {
         try {
+            const error = await this.validation.joiValidate(request);
+            if(error.message === `Internal server error`) {
+                return serverError(error);  //서버 메시지로 구분함
+            }
+            if(!!error) {
+                return badRequest(error);
+            }
+
+
             
+            return ok({message: "token"});
         }
         catch(err) {
-
+            return serverError(new ServerError());
         }
     }
 }
