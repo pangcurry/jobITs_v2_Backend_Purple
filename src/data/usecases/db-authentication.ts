888118cca968463f142a9ad6bc02e804870943d4
pangcurry <magicshuffle2424@hotmail.com>
2021-03-11 19:21:32 +0900

    [Add] add hashCompare in constructor and compare password

--- src/data/usecases/db-authentication.ts
+++ src/data/usecases/db-authentication.ts
@@ -1,9 +1,11 @@
 import { Authentication } from "../../domain/usecases";
+import { HashComparer } from "../protocols/cryptography";
 import { LoadUserByEmailRepository } from "../protocols/repository";
 
 export class DbAuthentication implements Authentication {
     constructor(
-        private readonly loadAccountByEmailRepository: LoadUserByEmailRepository
+        private readonly loadAccountByEmailRepository: LoadUserByEmailRepository,
+        private readonly hashComparer: HashComparer
         // private readonly
         // private readonly
         // private readonly
@@ -12,6 +14,13 @@ export class DbAuthentication implements Authentication {
     async auth(authenticationParams: Authentication.Params): Promise<Authentication.Result> {
         const user = await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.id);
         console.log(!!user);
+        if(user) {
+            const isVlid = await this.hashComparer.compare(authenticationParams.password, user.password);
+            if(isVlid) {
+                
+            }
+        }
+        
         return {
             accessToken: 'testid',
             refreshToken: 'testpassword'
