88152d23938ea97ad0aa817d01e97fb477cf8e1a
pangcurry <magicshuffle2424@hotmail.com>
2021-03-11 17:17:32 +0900

    [Add] add makeDbAuthentcation() in SigninController

--- src/main/factories/controllers/signin-controller-factory.ts
+++ src/main/factories/controllers/signin-controller-factory.ts
@@ -1,8 +1,9 @@
 import { SigninController } from "../../../presentation/controller";
 import { Controller } from "../../../presentation/protocols";
+import { makeDbAuthentication } from "../usecases";
 import { makeSigninValidation } from "../validations";
 // makeDbAuthentication(), 
 export const makeSigninController = (): Controller => {
-    return new SigninController(makeSigninValidation());
+    return new SigninController(makeDbAuthentication(), makeSigninValidation());
     // decorater로 변환하기
 };
