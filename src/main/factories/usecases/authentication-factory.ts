d6abd2a1af7a3437bc478a41b22b06c2e53d3a2f
pangcurry <magicshuffle2424@hotmail.com>
2021-03-11 20:13:59 +0900

    [Add] jwtAdapter in authentication factory

--- src/main/factories/usecases/authentication-factory.ts
+++ src/main/factories/usecases/authentication-factory.ts
@@ -1,11 +1,12 @@
 import { DbAuthentication } from "../../../data/usecases";
 import { Authentication } from "../../../domain/usecases";
-import { BcryptAdapter } from "../../../infra/cryptography";
+import { BcryptAdapter, JwtAdapter } from "../../../infra/cryptography";
 import { UserRepository } from "../../../infra/db/repositories";
 
 export const makeDbAuthentication = (): Authentication => {
     const salt = 19;    // env값으로 설정하기
     const bcryptAdapter = new BcryptAdapter(salt);
+    const jwtAdapter = new JwtAdapter();
     const accountRepository = new UserRepository();
     return new DbAuthentication(accountRepository, bcryptAdapter);
 }
