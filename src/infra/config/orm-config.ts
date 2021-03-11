c02c2864cc5858981c4d0d4601202b0cbbb4d538
pangcurry <magicshuffle2424@hotmail.com>
2021-03-11 17:06:55 +0900

    [Add] add orm-config

---
+++ src/infra/config/orm-config.ts
@@ -0,0 +1,18 @@
+// 추후 env 사용할 예정
+
+export const ormConfig =  {
+    port: 3306,
+    mysql: {
+      host: "jobits.cu895ra3thfv.ap-northeast-2.rds.amazonaws.com",
+      username: "jobits",
+      password: "jobitspw123!",
+      name: "jobits",
+    },
+    // secretKey: JWT_SECRET,
+    // s3: {
+    //   name: S3_NAME,
+    //   access: S3_ACCESS,
+    //   secret: S3_SECRET,
+    //   region: S3_REGION,
+    // },
+}
