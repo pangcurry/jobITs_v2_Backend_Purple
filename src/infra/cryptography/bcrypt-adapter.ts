cc6396ba17de39e486ced53e42be4abe38b52d8d
pangcurry <magicshuffle2424@hotmail.com>
2021-03-11 19:07:22 +0900

    [Add] add compare func in BcryptAdapter class

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,7 +1,11 @@
 import { HashComparer } from "../../data/protocols/cryptography";
 
+import bcrypt from 'bcrypt';
+
 export class BcryptAdapter implements HashComparer {
+    constructor(private readonly salt: number) {}
+
     async compare(plaintext: string, digest: string): Promise<boolean> {
-        
+        return bcrypt.compare(plaintext, digest);
     }
 }
