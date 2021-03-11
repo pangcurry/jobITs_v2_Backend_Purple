efd874cefe260001b08315e93fa1a00a7f8871f7
pangcurry <magicshuffle2424@hotmail.com>
2021-03-11 19:16:14 +0900

    [Add] add hasher implements

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,8 +1,8 @@
-import { HashComparer } from "../../data/protocols/cryptography";
+import { HashComparer, Hasher } from "../../data/protocols/cryptography";
 
 import bcrypt from 'bcrypt';
 
-export class BcryptAdapter implements HashComparer {
+export class BcryptAdapter implements Hasher ,HashComparer {
     constructor(private readonly salt: number) {}
 
     async hash(plaintext: string): Promise<string> {
