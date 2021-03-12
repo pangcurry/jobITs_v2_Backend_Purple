98378619531f7c926ffd938562e0f86d657ab3b7
pangcurry <magicshuffle2424@hotmail.com>
2021-03-12 17:10:44 +0900

    [Set] set code

--- src/main/routes/signin.ts
+++ src/main/routes/signin.ts
@@ -3,6 +3,6 @@ import { adaptRoute } from '../adapters';
 import { makeSigninController } from '../factories/controllers';
 
 export default (router: Router): void => {
-    router.post('/signin/basic',adaptRoute(makeSigninController()));
+    router.post('/signin/basic', adaptRoute(makeSigninController()));
     router.post('/signin/admin');
 }
