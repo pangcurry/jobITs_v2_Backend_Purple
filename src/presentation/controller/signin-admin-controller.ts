f552c9e5fb33006644b93d3f9a46132ac8103845
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 14:14:32 +0900

    [Add] add signinAdminController

--- src/presentation/controller/signin-admin-controller.ts
+++ src/presentation/controller/signin-admin-controller.ts
@@ -1,19 +1,32 @@
-import { badRequest, serverError } from "../helpers";
+import { Authentication } from "../../domain/usecases";
+import { ServerError } from "../errors";
+import { badRequest, ok, serverError } from "../helpers";
 import { Controller, HttpResponse, joiValidation } from "../protocols";
 
 export class SigninAdminController implements Controller {
     constructor(
+        private readonly authentication: Authentication,
         private readonly validation: joiValidation
     ) {}
     async handle(request: SigninAdminController.Request): Promise<HttpResponse> {
-        const error = await this.validation.joiValidate(request);
-        if(error.message === `Internal server error`) {
-            return serverError(error);
+        try {
+            const error = await this.validation.joiValidate(request);
+            if(error.message === `Internal server error`) {
+                return serverError(error);
+            }
+            if(!!error) {
+                return badRequest(error);
+            }
+            const authenticationModel = await this.authentication.auth(request);
+            console.log(authenticationModel);   // test loggin
+            if(authenticationModel.error) {
+                return authenticationModel.error;
+            }
+            return ok({ ...authenticationModel });
         }
-        if(!!error) {
-            return badRequest(error);
+        catch(err) {
+            return serverError(new ServerError());
         }
-        
     }
 }
 
