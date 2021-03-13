3a71d8034c8cf333729983c257481a53bdf1d04d
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 13:49:54 +0900

    [Add] add isValidAdminId, isValidAdminPw in joi adapter

--- src/infra/validators/joi-adapter.ts
+++ src/infra/validators/joi-adapter.ts
@@ -1,5 +1,9 @@
 import { schema } from '../config';
 
+// 함수 4개 하나로 합치기
+// 함수 4개 하나로 합치기
+// 함수 4개 하나로 합치기
+
 export class JoiAdapter {
     constructor() {}
     async isValidSId(sId: string): Promise<boolean> {
@@ -16,6 +20,28 @@ export class JoiAdapter {
     async isValidSPassword(sPassword: string): Promise<boolean> {
         try {
             await schema.validateAsync({ sPassword });
+            return true;
+        }
+        catch(err) {
+            console.log(err);   // error 로깅
+            return false;
+        }
+    };
+
+    async isValidAdminId(adminId: string): Promise<boolean> {
+        try {
+            await schema.validateAsync({ adminId });
+            return true;
+        }
+        catch(err) {
+            console.log(err);   // error 로깅
+            return false;
+        }
+    };
+
+    async isValidAdminPassword(adminPassword: string): Promise<boolean> {
+        try {
+            await schema.validateAsync({ adminPassword });
             return true;
         }
         catch(err) {
