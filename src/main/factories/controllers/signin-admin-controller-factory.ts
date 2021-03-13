b0d2fdd162517f03525dc214f54757032651f60b
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 13:52:20 +0900

    [Add] add make_signin_admin_validation func in make_signin_admin_controller

--- src/main/factories/controllers/signin-admin-controller-factory.ts
+++ src/main/factories/controllers/signin-admin-controller-factory.ts
@@ -1,5 +1,6 @@
 import { SigninAdminController } from "../../../presentation/controller";
 import { Controller } from "../../../presentation/protocols";
+import { makeSigninAdminValidation } from "../validations";
 
 export const makeSigninAdminController = (): Controller => {
     return new SigninAdminController();
