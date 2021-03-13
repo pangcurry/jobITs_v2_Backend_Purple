e215706912f8036977e698f9637fbac63baa82a5
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 17:25:33 +0900

    [Add] add authMiddleware, add verifier in constructor

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,9 +1,25 @@
+import { Verifier } from "../../data/protocols/cryptography";
+import { NoTokenError } from "../errors";
+import { badRequest, forbidden, ok } from "../helpers";
 import { HttpResponse, Middleware } from "../protocols";
 
 export class AuthMiddleware implements Middleware {
-    constructor() {}
+    constructor(
+        private readonly verifier: Verifier
+    ) {}
     async handle(request: AuthMiddleware.Request): Promise<HttpResponse> {
-        
+        const { accessToken } = request;
+        if(accessToken) {
+            const token = accessToken.split(' ')[1];
+            const verify = await this.verifier.verify(token);
+            const { error, decoded } = verify;
+            if(error) {
+                return forbidden(error);
+            }
+            return ok({ decoded });
+        }
+        console.log(accessToken);
+        return badRequest(new NoTokenError());
     }
 }
 
