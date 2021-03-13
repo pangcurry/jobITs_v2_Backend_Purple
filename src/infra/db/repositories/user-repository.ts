d65ff86ecf9e6ee4398ed6675e9596735d235f8e
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 14:06:05 +0900

    [Rename] rename email to id in loadById params

--- src/infra/db/repositories/user-repository.ts
+++ src/infra/db/repositories/user-repository.ts
@@ -2,13 +2,12 @@ import { EntityRepository, getRepository } from "typeorm";
 import { LoadUserByEmailRepository } from "../../../data/protocols/repository";
 import { User } from "../entities";
 
-
 @EntityRepository(User)
 export class UserRepository implements LoadUserByEmailRepository{
-    async loadByEmail(email: string): Promise<LoadUserByEmailRepository.Result> {
+    async loadById(id: string): Promise<LoadUserByEmailRepository.Result> {
         return await getRepository(User)
             .createQueryBuilder('user')
-            .where('user.id = :id', { id: email })
+            .where('user.id = :id', { id })
             .getOne();
     }
 }
