3b909b03edc67b1746c619c20f1419886c42f1de
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 12:45:41 +0900

    [Add] add signin/admin routes in routes/signin

--- src/main/routes/signin.ts
+++ src/main/routes/signin.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import { adaptRoute } from '../adapters';
-import { makeSigninController } from '../factories/controllers';
+import { makeSigninAdminController, makeSigninController } from '../factories/controllers';
 
 export default (router: Router): void => {
     router.post('/signin/basic', adaptRoute(makeSigninController()));
-    router.post('/signin/admin');
+    router.post('/signin/admin', adaptRoute(makeSigninAdminController()));
 }
