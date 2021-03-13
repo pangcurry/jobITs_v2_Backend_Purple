e1b6c46ea99072ab92e752924319dad5f0763a09
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 17:27:45 +0900

    [Add] add express middleware adapter

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -1,5 +1,20 @@
 import { Middleware } from "../../presentation/protocols";
 
+import { Request, Response, NextFunction } from 'express';
+
 export const adaptMiddleware = (middleware: Middleware) => {
-    
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const request = {
+            accessToken: req.headers?.["authorization"],
+            ...(req.headers || {})
+        };
+        const httpResponse = await middleware.handle(request);
+        const { status, statusCode, message, data } = httpResponse;
+        if(200 === status) {
+            Object.assign(req, data);
+            next();
+        } else {
+            res.status(status).json({ statusCode, error: message });
+        }
+    }
 }
