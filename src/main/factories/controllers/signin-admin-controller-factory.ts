64ff4832c455732078a8fc1330529598d3f43e5b
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 14:14:02 +0900

    [Add] add makeDbauthentcation func in signinAdminController

--- src/main/factories/controllers/signin-admin-controller-factory.ts
+++ src/main/factories/controllers/signin-admin-controller-factory.ts
@@ -1,7 +1,8 @@
 import { SigninAdminController } from "../../../presentation/controller";
 import { Controller } from "../../../presentation/protocols";
+import { makeDbAuthentication } from "../usecases";
 import { makeSigninAdminValidation } from "../validations";
 
 export const makeSigninAdminController = (): Controller => {
-    return new SigninAdminController(makeSigninAdminValidation());
+    return new SigninAdminController(makeDbAuthentication() , makeSigninAdminValidation());
 }
