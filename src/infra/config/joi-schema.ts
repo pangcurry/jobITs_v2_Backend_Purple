e18de20013235ad05b6122c4bc5a8dc04cecc81d
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 14:35:45 +0900

    [remove] remove alphanum()

--- src/infra/config/joi-schema.ts
+++ src/infra/config/joi-schema.ts
@@ -8,6 +8,6 @@ const admin_config = {  // 추후 env처리
 export const schema = Joi.object().keys({
     sId:Joi.string().min(7).email({ minDomainSegments:3, tlds: { allow: ['kr'] } }),
     sPassword: Joi.string().min(6).max(20),
-    adminId:Joi.string().alphanum().min(admin_config.numOfName).max(admin_config.numOfName),
+    adminId:Joi.string().min(admin_config.numOfName).max(admin_config.numOfName),
     adminPassword: Joi.string().min(6).max(20)
 });
