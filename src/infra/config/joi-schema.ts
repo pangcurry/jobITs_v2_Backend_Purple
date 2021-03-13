ec488821dfd9a45f368e3de20cdba549e6cdb91e
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 13:27:05 +0900

    [Add] add adminId, adminPassowrd, admin_config in scema

--- src/infra/config/joi-schema.ts
+++ src/infra/config/joi-schema.ts
@@ -1,6 +1,13 @@
 import Joi from 'joi';
 
+const admin_config = {  // 추후 env처리
+    name: "JOBITS_ADMIN",
+    numOfName: 12
+}
+
 export const schema = Joi.object().keys({
     sId:Joi.string().min(7).email({ minDomainSegments:3, tlds: { allow: ['kr'] } }),
-    sPassword: Joi.string().min(6).max(20)
+    sPassword: Joi.string().min(6).max(20),
+    adminId:Joi.string().alphanum().min(admin_config.numOfName).max(admin_config.numOfName),
+    adminPassword: Joi.string().min(6).max(20)
 });
