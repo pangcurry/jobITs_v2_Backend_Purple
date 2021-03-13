b365bad9d2347bfc897c177ca4443cb21ac9b0c8
pangcurry <magicshuffle2424@hotmail.com>
2021-03-13 17:32:58 +0900

    [Add] add auth middleware and makeInfoController function in /info/name

--- src/main/routes/info.ts
+++ src/main/routes/info.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { adaptRoute } from '../adapters';
+import { makeInfoController } from '../factories/controllers';
+import { auth } from '../middlewares';
 
 export default (router: Router): void => {
-    router.get('/info/name', adaptRoute());
+    // router.get('/info/name', adaptRoute(makeInfoController()));
+    router.get('/info/name', auth, adaptRoute(makeInfoController()));
 }
