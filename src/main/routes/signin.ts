3329fad27ac37f957b26b9c072120d5511166320
pangcurry <magicshuffle2424@hotmail.com>
2021-03-14 23:47:51 +0900

    [Add] add route signin/password

--- src/main/routes/signin.ts
+++ src/main/routes/signin.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { adaptRoute } from '../adapters';
-import { makeSigninAdminController, makeSigninController } from '../factories/controllers';
+import { makeSigninAdminController, makeSigninController, makeSigninPasswordController } from '../factories/controllers';
+import { auth } from '../middlewares';
 
 export default (router: Router): void => {
     router.post('/signin/basic', adaptRoute(makeSigninController()));
     router.post('/signin/admin', adaptRoute(makeSigninAdminController()));
-    router.post('/signin/password', adaptRoute());
+    router.post('/signin/password', auth, adaptRoute(makeSigninPasswordController()));
 }
