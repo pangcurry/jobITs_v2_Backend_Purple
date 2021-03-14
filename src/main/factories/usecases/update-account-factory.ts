0c902883df4c0e07966161b0626fb9f71ab14260
pangcurry <magicshuffle2424@hotmail.com>
2021-03-14 23:45:21 +0900

    [Add] add dbUpdateAccount factory

---
+++ src/main/factories/usecases/update-account-factory.ts
@@ -0,0 +1,11 @@
+import { DbPasswordAuthentication, DbUpdateAccount } from "../../../data/usecases";
+import { PasswordAuthentication, UpdateAccount } from "../../../domain/usecases";
+import { BcryptAdapter } from "../../../infra/cryptography";
+import { UserRepository } from "../../../infra/db/repositories";
+
+export const makeDbUpdateAccount = (): UpdateAccount => {
+    // const salt = 19;    // env값으로 설정하기
+    // const accountRepository = new UserRepository();
+    // const bcryptAdapter = new BcryptAdapter(salt);
+    return new DbUpdateAccount();
+}
