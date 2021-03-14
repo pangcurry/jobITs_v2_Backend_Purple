f30c8ba55fc9264cdcf7f49b83b9d29fee1a5e5d
pangcurry <magicshuffle2424@hotmail.com>
2021-03-14 23:46:45 +0900

    [Add] add signinPasswordController factory

---
+++ src/main/factories/controllers/signin-password-controller-factory.ts
@@ -0,0 +1,8 @@
+import { SigninPasswordController } from "../../../presentation/controller";
+import { Controller } from "../../../presentation/protocols";
+import { makeDbPasswordAuthentication, makeDbUpdateAccount } from "../usecases";
+import { makeSigninPasswordValidation } from "../validations";
+
+export const makeSigninPasswordController = (): Controller => {
+    return new SigninPasswordController(makeSigninPasswordValidation(), makeDbPasswordAuthentication(),  makeDbUpdateAccount());
+}
