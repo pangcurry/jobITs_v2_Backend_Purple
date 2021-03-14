bdfa4f844d7ff74e3115045ca6338f38674a3554
pangcurry <magicshuffle2424@hotmail.com>
2021-03-14 21:30:50 +0900

    [Add] add InfoController

--- src/presentation/controller/info-controller.ts
+++ src/presentation/controller/info-controller.ts
@@ -1,8 +1,26 @@
+import { LoadUserById } from "../../domain/usecases";
+import { ok } from "../helpers";
 import { Controller, HttpResponse } from "../protocols";
 
-export class infoController implements Controller {
-    constructor() {}
-    async handle(): Promise<HttpResponse> {
-           
+export class InfoController implements Controller {
+    constructor(private readonly loadUserById: LoadUserById) {}
+    async handle(request:InfoController.Request): Promise<HttpResponse> {
+        const user = await this.loadUserById.load(request.decoded.user_id);
+        const { name, error } = user;
+        if(error) {
+            return error;
+        }
+        return ok({ name });
+    }
+}
+
+export namespace InfoController {
+    export type Request = {
+        decoded: {
+            issuer: string,
+            expiresIn: string,
+            user_id: string,
+            isAdmin: boolean
+        }
     }
 }
