08a166abb1c319549a359d758654a7f76fcc0a3e
pangcurry <magicshuffle2424@hotmail.com>
2021-03-14 23:50:39 +0900

    [Add] add path password authentication factory, update account factory

--- src/main/factories/usecases/index.ts
+++ src/main/factories/usecases/index.ts
@@ -1,2 +1,4 @@
 export * from './authentication-factory';
 export * from './load-info-by-user-factory';
+export * from './password-authentication-factory';
+export * from './update-account-factory';
