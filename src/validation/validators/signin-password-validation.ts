71e6f01e1c55006bdccae38c81e9ef4654fef778
pangcurry <magicshuffle2424@hotmail.com>
2021-03-14 23:40:53 +0900

    [Add] add signinPasswordValidation validator

---
+++ src/validation/validators/signin-password-validation.ts
@@ -0,0 +1,34 @@
+import { JoiAdapter } from "../../infra/validators";
+import { SigninAdminController } from "../../presentation/controller";
+import { InvalidParamError, ServerError } from "../../presentation/errors";
+import { joiValidation } from "../../presentation/protocols";
+
+export class SigninPasswordValidation implements joiValidation {
+    constructor(
+        private readonly validation: JoiAdapter
+    ) {}
+    async joiValidate(input:SigninPasswordValidation.Request): Promise<Error> {
+        try {
+            const isValid_oldPw = await this.validation.isValidAdminPassword(input.oldPw);
+            const isValid_newPw = await this.validation.isValidAdminPassword(input.newPw);
+            if(!isValid_oldPw) {
+                return new InvalidParamError('old password');
+            }
+            if(!isValid_newPw) {
+                return new InvalidParamError('new password');
+            }
+            return false as any;
+        }
+        catch(err) {
+            console.log(err);
+            return new ServerError();
+        }
+    }
+}
+
+export namespace SigninPasswordValidation {
+    export type Request = {
+        oldPw: string,
+        newPw: string
+    }
+}
