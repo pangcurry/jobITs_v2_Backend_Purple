99aa796b09451cb2002fadc9d1b3b1a6b9289fc3
pangcurry <magicshuffle2424@hotmail.com>
2021-03-15 10:22:31 +0900

    [Add] add await in hash func

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -6,7 +6,7 @@ export class BcryptAdapter implements Hasher ,HashComparer {
     constructor(private readonly salt: number) {}
 
     async hash(plaintext: string): Promise<string> {
-        return bcrypt.hash(plaintext,this.salt);
+        return  await bcrypt.hash(plaintext, this.salt);
     }
 
     async compare(plaintext: string, digest: string): Promise<boolean> {
