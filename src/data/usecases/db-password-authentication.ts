c9c9e2ae03ee375b961264638939206633ce2303
pangcurry <magicshuffle2424@hotmail.com>
2021-03-15 10:24:19 +0900

    [Add] add isValid

--- src/data/usecases/db-password-authentication.ts
+++ src/data/usecases/db-password-authentication.ts
@@ -1,7 +1,7 @@
 import { PasswordAuthentication } from "../../domain/usecases";
 import { AuthenticationFailureError, UserNotFoundError } from "../../presentation/errors";
 import { forbidden, notFound } from "../../presentation/helpers";
-import { Encrypter, HashComparer } from "../protocols/cryptography";
+import { HashComparer } from "../protocols/cryptography";
 import { LoadUserByEmailRepository } from "../protocols/repository";
 
 // const accessToken_config = {
@@ -28,8 +28,8 @@ export class DbPasswordAuthentication implements PasswordAuthentication {
         const user = await this.loadAccountByEmailRepository.loadById(authenticationParams.id);
         console.log(!!user);    // test logging
         if(user) {
-            // const isValid = await this.hashComparer.compare(authenticationParams.oldPw, user.password);
-            const isValid = true;    // 임시로 비번 오픈
+            const isValid = await this.hashComparer.compare(authenticationParams.oldPw, user.password);
+            // const isValid = true;    // 임시로 비번 오픈
             if(isValid) {
                 return null;
             }
