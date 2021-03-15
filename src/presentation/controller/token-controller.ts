9e8a4f8f436ae714327fab55e7568371a0f11452
pangcurry <magicshuffle2424@hotmail.com>
2021-03-15 14:09:10 +0900

    [Add] add token authentication in token controller

--- src/presentation/controller/token-controller.ts
+++ src/presentation/controller/token-controller.ts
@@ -9,15 +9,13 @@ export class TokenController implements Controller {
     async handle(request: TokenController.Request): Promise<HttpResponse> {
         // 리프레시 토큰과 액세스 토큰의 페이로드가 같은가 ex) user_id, isAdmin
         const { decoded, refreshToken } = request;
-        const auth = await this.authentication.auth({ id: decoded.user_id, refreshToken });
-        if(auth.error) {
-            return auth.error;
+        console.log(request);
+        const token = await this.authentication.auth({ id: decoded.user_id, refreshToken });
+        if(token.error) {
+            return token.error;
         }
         // 새로운 토큰 발급
-        return ok({ 
-            accessToken: auth.accessToken,
-            refreshToken: auth.refreshToken
-        });
+        return ok(token);
     }
 }
 
