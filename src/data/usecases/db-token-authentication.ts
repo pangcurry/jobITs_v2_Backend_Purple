0589353cdb242fed187ab1a6cf2e87c4796a7fa9
pangcurry <magicshuffle2424@hotmail.com>
2021-03-15 14:06:00 +0900

    [Add] add dbTokenauthentication

--- src/data/usecases/db-token-authentication.ts
+++ src/data/usecases/db-token-authentication.ts
@@ -1,8 +1,8 @@
 import { Authentication, TokenAuthentication } from "../../domain/usecases";
-import { AuthenticationFailureError, UserNotFoundError } from "../../presentation/errors";
+import { AnotherTokenError, AuthenticationFailureError, InvalidRefreshTokenError, UserNotFoundError } from "../../presentation/errors";
 import { forbidden, notFound } from "../../presentation/helpers";
 import { Encrypter, HashComparer, Verifier } from "../protocols/cryptography";
-import { LoadUserByEmailRepository } from "../protocols/repository";
+import { CheckAccountByIdRepository, LoadUserByEmailRepository } from "../protocols/repository";
 
 const accessToken_config = {
     issuer: 'JobITs',
@@ -23,41 +23,40 @@ export class DbTokenAuthentication implements TokenAuthentication {
         // private readonly loadAccountByEmailRepository: LoadUserByEmailRepository,
         // private readonly hashComparer: HashComparer,
         private readonly jwtEncrypter: Encrypter,
-        private readonly jwtVerifier: Verifier
+        private readonly jwtVerifier: Verifier,
+        private readonly repository: CheckAccountByIdRepository
         
     ) {}
 
     async auth(authenticationParams: TokenAuthentication.Params): Promise<TokenAuthentication.Result> {
+        const { id, refreshToken } = authenticationParams;
         // 리프레시 토큰이 유효한가?
-        // decoded.id 와 리프레시 토큰의 아이디가 같은가
-        // decoded.id 가 db에 존재하는가
-        // /decoded.id 와 JOBITS_ADMIN이 같은가 -> isAdmin 설정
-        
-
-        const user = await this.loadAccountByEmailRepository.loadById(authenticationParams.id);
-        console.log(!!user);    // test logging
-        if(user) {
-            const isVlid = await this.hashComparer.compare(authenticationParams.password, user.password);
-            // const isVlid = true;    // 임시로 비번 오픈
-            if(isVlid) {
-                let isAdmin: boolean = false;
-                if(user.name === admin_config.name) {
-                    isAdmin = true;
-                }
-                const accessToken = await this.jwtEncrypter.encrypt({
-                    ...accessToken_config,
-                    user_id: user.id,
-                    isAdmin
-                });
-                const refreshToken = await this.jwtEncrypter.encrypt({
-                    ...refreshToken_config,
-                    user_id: user.id,
-                    isAdmin
-                });
-                return { accessToken, refreshToken };
-            }
-            return { error: forbidden(new AuthenticationFailureError()) }
+        const refresh_id = await this.jwtVerifier.verify(refreshToken);
+        if(refresh_id.error) {
+            return { error: forbidden(new InvalidRefreshTokenError()) };
         }
-        return { error: notFound(new UserNotFoundError()) };
+        // decoded.id 와 리프레시 토큰의 아이디가 같은가
+        if(refresh_id.decoded.user_id !== id) {
+            return { error: forbidden(new AnotherTokenError()) };
+        }
+        // decoded.id 가 db에 존재하는가
+        const isExist = this.repository.checkById(id);
+        // isExist 에서 값을 찾을 수 없을 때 null이 나오는지 확인하기
+        if(!isExist) {
+            return { error: notFound(new UserNotFoundError()) }
+        }
+        // /decoded.id 와 JOBITS_ADMIN이 같은가 -> isAdmin 설정
+        let isAdmin: boolean = (id === admin_config.name);
+        const access_Token = await this.jwtEncrypter.encrypt({
+            ...accessToken_config,
+            user_id: id,
+            isAdmin
+        });
+        const refresh_Token = await this.jwtEncrypter.encrypt({
+            ...refreshToken_config,
+            user_id: id,
+            isAdmin
+        });
+        return { accessToken: access_Token, refreshToken: refresh_Token }
     }
 }
