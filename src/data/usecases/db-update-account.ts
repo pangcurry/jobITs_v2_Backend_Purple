2836ed42da5a92c7a567db4e5a41ed512308e721
pangcurry <magicshuffle2424@hotmail.com>
2021-03-15 10:25:06 +0900

    [Add] add dbUpdateAccount class

--- src/data/usecases/db-update-account.ts
+++ src/data/usecases/db-update-account.ts
@@ -1,9 +1,27 @@
 import { UpdateAccount } from "../../domain/usecases";
+import { ServerError } from "../../presentation/errors";
+import { serverError } from "../../presentation/helpers";
+import { Hasher } from "../protocols/cryptography";
+import { UpdateUserPassowrdRepository } from "../protocols/repository";
 
 export class DbUpdateAccount implements UpdateAccount {
-  constructor () {}
+  constructor (
+      private readonly updateUserPasswordRepository: UpdateUserPassowrdRepository,
+      private readonly hasher: Hasher
+  ) {}
 
   async update(account: UpdateAccount.Params): Promise<UpdateAccount.Result> {
       // 여기서부터 db에 비번 update 로직 짜기
+      try {
+        const { id, password } = account;
+        const hash = await this.hasher.hash(password);
+        await this.updateUserPasswordRepository.updatePassword(id, hash);
+        return null as any;
+      }
+      catch(err) {
+        console.log(err);
+          return { error: serverError(new ServerError()) };
+      }
+      
   }
 }
