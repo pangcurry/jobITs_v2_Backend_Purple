0f6acfc3d2e442282baa08d4e82618b1f914ac26
pangcurry <magicshuffle2424@hotmail.com>
2021-03-15 15:27:47 +0900

    [Add] add dev /encryption route

---
+++ src/main/routes/encryption.ts
@@ -0,0 +1,12 @@
+import { Request, Response, Router } from 'express';
+import { BcryptAdapter } from '../../infra/cryptography';
+
+export default (router: Router): void => {
+    router.get('/encryption', async (req: Request, res: Response) => {
+        const bcryptAdapter = new BcryptAdapter(3);
+        const chipherText: string = await bcryptAdapter.hash(req.body.text);
+        res.status(200).json({
+            chipherText: chipherText
+        })
+    });
+}
