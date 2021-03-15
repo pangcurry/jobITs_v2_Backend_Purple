f1b45c6b4a4fb8df5f8792577f5f01b5eae34103
pangcurry <magicshuffle2424@hotmail.com>
2021-03-15 10:21:15 +0900

    [Add] add updatePassword

--- src/infra/db/repositories/user-repository.ts
+++ src/infra/db/repositories/user-repository.ts
@@ -1,13 +1,24 @@
 import { EntityRepository, getRepository } from "typeorm";
-import { LoadUserByEmailRepository } from "../../../data/protocols/repository";
+import { LoadUserByEmailRepository, UpdateUserPassowrdRepository } from "../../../data/protocols/repository";
 import { User } from "../entities";
 
 @EntityRepository(User)
-export class UserRepository implements LoadUserByEmailRepository{
+export class UserRepository implements LoadUserByEmailRepository, UpdateUserPassowrdRepository{
     async loadById(id: string): Promise<LoadUserByEmailRepository.Result> {
         return await getRepository(User)
             .createQueryBuilder('user')
             .where('user.id = :id', { id })
             .getOne();
+    }
+
+    async updatePassword(id: string, password: string): Promise<any> {
+        return await getRepository(User)
+            .createQueryBuilder('user')
+            .update(User)
+            .set({
+                password
+            })
+            .where('user.id = :id', { id })
+            .execute();
     }
 }
