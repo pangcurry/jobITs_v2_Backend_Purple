bcd1e54501040924639d232a4ab34cb8252a3072
pangcurry <magicshuffle2424@hotmail.com>
2021-03-15 10:37:10 +0900

    [Update] update path

--- src/main/factories/controllers/index.ts
+++ src/main/factories/controllers/index.ts
@@ -1,4 +1,7 @@
 export * from './signin-controller-factory';
 export * from './signin-admin-controller-factory';
-export * from './info-controller-factory';
 export * from './signin-password-controller-factory';
+
+export * from './info-controller-factory';
+
+export * from './token-controller-factory';
--- src/presentation/controller/index.ts
+++ src/presentation/controller/index.ts
@@ -3,3 +3,5 @@ export * from './signin-admin-controller';
 export * from './signin-password-controller';
 
 export * from './info-controller';
+
+export * from './token-controller';
