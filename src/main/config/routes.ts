70c2667737f7b424e66d8680669e5f5be273b9b2
pangcurry <magicshuffle2424@hotmail.com>
2021-03-16 09:11:31 +0900

    [Add] add routes encryption

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,12 +4,14 @@ import signinRoutes from '../routes/signin';
 import infoRoutes from '../routes/info';
 import tokenRoutes from '../routes/token';
 import recruitRoutes from '../routes/recruit';
+import encryptionRoutes from '../routes/encryption';
 
 const router = Router();
 
 export default (app: Express): void => {
     app.use('/', router);
 
+    encryptionRoutes(router);
     signinRoutes(router);
     infoRoutes(router);
     tokenRoutes(router);
---
+++ src/main/routes/encryption.ts
@@ -0,0 +1,15 @@
+import { Request, Response, Router } from 'express';
+
+import bcrypt from 'bcrypt';
+
+export default (router: Router): void => {
+    router.get('/encryption', async (req:Request, res: Response) => {
+        console.log(req.body.plaintext);
+        // const bcrypt = new BcryptAdapter(3);
+        const text = await bcrypt.hash("dddddd", 3);
+        console.log(text);
+        res.status(200).json({
+            text: text
+        })
+    });
+}
