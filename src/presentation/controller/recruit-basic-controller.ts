f74ce91475eb11b5f332ff46f21b9d30265d6285
pangcurry <magicshuffle2424@hotmail.com>
2021-03-17 11:19:28 +0900

    [Add] add recruitBasicController class

---
+++ src/presentation/controller/recruit-basic-controller.ts
@@ -0,0 +1,25 @@
+import { LoadSimpleEnterprises } from "../../domain/usecases";
+import { ServerError } from "../errors";
+import { notFound, ok, serverError } from "../helpers";
+import { Controller, HttpResponse } from "../protocols";
+
+export class RecruitBasicController implements Controller {
+    constructor(
+        private readonly loadSimpleEnterprises: LoadSimpleEnterprises
+    ) {}
+    async handle(): Promise<HttpResponse> {
+        try {
+            const data = await this.loadSimpleEnterprises.load();
+            const { error, list } = data;
+            if(error) {
+                return notFound(error);
+            }
+            return ok({ list });
+            // reception을 기준으로 db에서 목록 가지고 오기
+        }
+        catch(err) {
+            console.log(err);
+            return serverError(new ServerError());
+        }
+    }
+}
