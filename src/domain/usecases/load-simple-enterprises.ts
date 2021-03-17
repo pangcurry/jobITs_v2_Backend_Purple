a65ba0a17d13e9239fdede34024335aaa80f4944
pangcurry <magicshuffle2424@hotmail.com>
2021-03-17 11:17:09 +0900

    [Add] add loadSimpleEnterprises interface

---
+++ src/domain/usecases/load-simple-enterprises.ts
@@ -0,0 +1,18 @@
+export interface LoadSimpleEnterprises {
+    load:() => Promise<LoadSimpleEnterprises.Result>
+    // 인자로 sortBy 나중에 넣기
+}
+
+export namespace LoadSimpleEnterprises {
+    export type Enterprise = {
+        recruit_deadline: string,
+        enterprise_name: string,
+        enterprise_introduce: string,
+        specialty_specialty: string,
+        recruit_id: string
+    }
+    export type Result = {
+        list: LoadSimpleEnterprises.Enterprise[],
+        error?: Error
+    }
+}
