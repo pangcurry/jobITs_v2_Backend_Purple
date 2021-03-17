373d13a5d13f245fd67a343dae4ddeb3d1d2d6de
pangcurry <magicshuffle2424@hotmail.com>
2021-03-17 11:20:34 +0900

    [Add] add auth middleware, RecruitBasiccontroller factory func

--- src/main/routes/recruit.ts
+++ src/main/routes/recruit.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import { adaptRoute } from '../adapters';
+import { makeRecruitBasicController } from '../factories/controllers';
+import { auth } from '../middlewares';
 
 export default (router: Router): void => {
-    router.get('/recruit/basic');
+    router.get('/recruit/basic', auth, adaptRoute(makeRecruitBasicController()));
     router.get('/recruit/search');
 }
