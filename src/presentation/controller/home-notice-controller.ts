08400b5c04daf4c7fab89f2eda091ccc8f1c22a2
pangcurry <magicshuffle2424@hotmail.com>
2021-03-17 20:27:27 +0900

    [Add] add homeNoticeController class

---
+++ src/presentation/controller/home-notice-controller.ts
@@ -0,0 +1,8 @@
+import { Controller, HttpResponse } from "../protocols";
+
+export class HomeNoticeController implements Controller {
+    constructor() {}
+    async handle(): Promise<HttpResponse> {
+
+    }
+}
