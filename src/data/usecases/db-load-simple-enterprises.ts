ebec3008c74100dbafc59e2286f6ce247dc14c38
pangcurry <magicshuffle2424@hotmail.com>
2021-03-17 11:17:49 +0900

    [Add] add dbLoadSimpleEnterprises class

---
+++ src/data/usecases/db-load-simple-enterprises.ts
@@ -0,0 +1,26 @@
+import { LoadSimpleEnterprises } from "../../domain/usecases";
+import { RecruitRepository } from "../../infra/db/repositories";
+import { NoListError, ServerError } from "../../presentation/errors";
+
+export class DbLoadSimpleEnterprises implements LoadSimpleEnterprises {
+    constructor(
+        private readonly recruitRepository: RecruitRepository
+    ) {}
+    async load(): Promise<LoadSimpleEnterprises.Result> {
+        try {
+            const list = await this.recruitRepository.load();
+            if(list === []) {
+                return {
+                    list: [],
+                    error: new NoListError()
+                };
+            }
+            console.log(list);
+            return { list };
+        }
+        catch(err) {
+            console.log(err);   // 에러 로깅
+            return { list: [], error: new ServerError() };
+        }
+    }
+}
