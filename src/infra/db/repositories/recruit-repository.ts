a2534549a2c46c817d663e75aa2c2ae89ed31fb5
pangcurry <magicshuffle2424@hotmail.com>
2021-03-18 18:04:04 +0900

    [Add] add loadHomeNoticesRecruit func in recruit-repository

--- src/infra/db/repositories/recruit-repository.ts
+++ src/infra/db/repositories/recruit-repository.ts
@@ -1,17 +1,26 @@
 import { EntityRepository, getRepository } from "typeorm";
-import { LoadSimpleEnterprisesRepository } from "../../../data/protocols/repository/recruit";
+import { LoadHomeNoticesRecruitRepository, LoadSimpleEnterprisesRepository } from "../../../data/protocols/repository/recruit";
 import { Recruit } from "../entities";
 
 @EntityRepository(Recruit)
-export class RecruitRepository implements LoadSimpleEnterprisesRepository {
+export class RecruitRepository implements LoadSimpleEnterprisesRepository, LoadHomeNoticesRecruitRepository {
     async load(): Promise<LoadSimpleEnterprisesRepository.Result> {
         return await getRepository(Recruit)
             .createQueryBuilder('recruit')
             .innerJoin('enterprise', 'enterprise', 'recruit.ent_no = enterprise.ent_no')
             .innerJoin('qualification', 'qualification', 'recruit.recruit_id = qualification.recruit_id')
-            .innerJoin('specialty', 'specialty', 'qualification.qualification_id = specialty.qualification_id')
-            .select(['recruit.recruit_id', 'enterprise.introduce', 'enterprise.name', 'specialty.specialty', 'recruit.deadline'])
+            // .innerJoin('specialty', 'specialty', 'qualification.qualification_id = specialty.qualification_id')
+            .select(['recruit.recruit_id', 'enterprise.introduce', 'enterprise.name', 'qualification.specialty', 'recruit.deadline'])
             .orderBy('recruit.reception', 'DESC')
+            .getRawMany();
+    }
+    async loadHomeNoticesRecruit(): Promise<LoadHomeNoticesRecruitRepository.Result> {
+        return await getRepository(Recruit)
+            .createQueryBuilder('recruit')
+            .innerJoin('enterprise', 'enterprise', 'recruit.ent_no = enterprise.ent_no')
+            .select(['recruit.recruit_no', 'recruit.reception', 'enterprise.name'])
+            .orderBy('recruit.recruit_no', 'DESC')
+            .limit(5)
             .getRawMany();
     }
 }
