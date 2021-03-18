9da3b2637f40ea627cc6a0ccca5fd30e75730322
pangcurry <magicshuffle2424@hotmail.com>
2021-03-18 18:06:22 +0900

    [Add] add homeNoticeController

--- src/presentation/controller/home-notice-controller.ts
+++ src/presentation/controller/home-notice-controller.ts
@@ -1,8 +1,27 @@
+import { LoadHomeNotices } from "../../domain/usecases";
+import { ServerError } from "../errors";
+import { notFound, ok, serverError } from "../helpers";
 import { Controller, HttpResponse } from "../protocols";
 
 export class HomeNoticeController implements Controller {
-    constructor() {}
+    constructor(
+        private readonly loadHomeNotices: LoadHomeNotices
+    ) {}
     async handle(): Promise<HttpResponse> {
-        
+        try {
+            const data = await this.loadHomeNotices.load();
+            const { error } = data;
+            if(error) {
+                if(error.message === `Internal server error`) {
+                    return serverError(error);
+                }
+                return notFound(error);
+            }
+            return ok({ ...data });
+        }
+        catch(err) {
+            console.log(err);
+            return serverError(new ServerError());
+        }
     }
 }
