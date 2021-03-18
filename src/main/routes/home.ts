3b7442a8d763bf64471409aeaf8d1d8541403470
pangcurry <magicshuffle2424@hotmail.com>
2021-03-18 18:08:07 +0900

    [Add] add routes /home/notice

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters";
+import { makeHomeNoticeController } from "../factories/controllers";
+import { auth } from "../middlewares";
 
 export default (router: Router): void => {
-    router.get('/home/notice', adaptRoute());
+    router.get('/home/notice', auth, adaptRoute(makeHomeNoticeController()));
 }
