ad6b66bb736f1c656e65b3d3639f9607f05017ab
pangcurry <magicshuffle2424@hotmail.com>
2021-03-18 18:06:54 +0900

    [Add] add homeNoticeController factory

--- src/main/factories/controllers/home-notice-controller-factory.ts
+++ src/main/factories/controllers/home-notice-controller-factory.ts
@@ -1,6 +1,7 @@
 import { HomeNoticeController } from "../../../presentation/controller";
 import { Controller } from "../../../presentation/protocols";
+import { makeDbLoadHomeNotices } from "../usecases";
 
 export const makeHomeNoticeController = (): Controller => {
-    return new HomeNoticeController();
+    return new HomeNoticeController(makeDbLoadHomeNotices());
 }
