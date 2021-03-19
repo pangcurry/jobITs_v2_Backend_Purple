5b46189e207a6900d804839a4106c3b29d4786df
pangcurry <magicshuffle2424@hotmail.com>
2021-03-19 09:46:22 +0900

    [Remove] Remove auth middleware in /home/notice

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters";
 import { makeHomeNoticeController } from "../factories/controllers";
-import { auth } from "../middlewares";
 
 export default (router: Router): void => {
-    router.get('/home/notice', auth, adaptRoute(makeHomeNoticeController()));
+    router.get('/home/notice', adaptRoute(makeHomeNoticeController()));
 }
