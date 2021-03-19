5e7b84aebc5676adaab193c236007e8242cebf95
pangcurry <magicshuffle2424@hotmail.com>
2021-03-19 09:43:54 +0900

    [Add] add routes /recruit/search

--- src/main/routes/recruit.ts
+++ src/main/routes/recruit.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { adaptRoute } from '../adapters';
-import { makeRecruitBasicController } from '../factories/controllers';
+import { makeRecruitBasicController, makeRecruitSearchController } from '../factories/controllers';
 import { auth } from '../middlewares';
 
 export default (router: Router): void => {
     router.get('/recruit/basic', auth, adaptRoute(makeRecruitBasicController()));
-    router.get('/recruit/search');
+    router.get('/recruit/search', adaptRoute(makeRecruitSearchController()));
 }
