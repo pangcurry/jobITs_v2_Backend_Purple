fc5f669414a6efb397c9b61550097f651285fd39
pangcurry <magicshuffle2424@hotmail.com>
2021-04-14 09:56:27 +0900

    [Fix] fix salt config

--- src/main/factories/usecases/update-account-factory.ts
+++ src/main/factories/usecases/update-account-factory.ts
@@ -2,9 +2,11 @@ import { DbPasswordAuthentication, DbUpdateAccount } from "../../../data/usecase
 import { PasswordAuthentication, UpdateAccount } from "../../../domain/usecases";
 import { BcryptAdapter } from "../../../infra/cryptography";
 import { UserRepository } from "../../../infra/db/repositories";
+import env from '../../config/env';
 
 export const makeDbUpdateAccount = (): UpdateAccount => {
-    const salt = 3;    // env값으로 설정하기
+    const salt = env.bcrypt.salt;
+    // const salt = 3;    // env값으로 설정하기
     const accountRepository = new UserRepository();
     const bcryptAdapter = new BcryptAdapter(salt);
     return new DbUpdateAccount(accountRepository, bcryptAdapter);
